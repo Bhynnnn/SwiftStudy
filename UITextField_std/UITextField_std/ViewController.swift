74ef96a9df3254ae4ceb2f42918571e7129d2a13
bhyn98 <bhyn9785@gmail.com>
2023-01-05 16:55:20 +0900

    First commit

---
+++ UITextField_std/UITextField_std/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UITextField_std
+//
+//  Created by 강보현 on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    private let
+
+    func btnAction(_ sender: UIButton) {
+        label.text = textField.text
+    }
+}
+
